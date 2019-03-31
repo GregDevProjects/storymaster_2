758a9f9674892f7287a883e84ec9b264afd4a058
GregDevProjects <gdonaldsonmclean@gmail.com>
2019-03-31 08:54:05 -0300

    add linting for client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react';
-import './App.css';
-import * as firebase from 'firebase';
+import React, { Component } from 'react'
+import './App.css'
+import * as firebase from 'firebase'
 import moment from 'moment'
 
-import {CountdownTimer} from './components/CountdownTimer'
+import { CountdownTimer } from './components/CountdownTimer'
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            roundTimeLeft: '',
-            roundNumber: ''
-        }
+  constructor () {
+    super()
+    this.state = {
+      roundTimeLeft: '',
+      roundNumber: ''
     }
+  }
 
-    componentDidMount() {
-        const timeRef = firebase.database().ref().child('next_round_start');
-        const roundNumberRef = firebase.database().ref().child('round_number');
-        timeRef.on('value', snap => 
-            this.setState({roundTimeLeft : snap.val()})
-        );
-        roundNumberRef.on('value', snap => 
-            this.setState({roundNumber: snap.val()})
-        );
-    }
+  componentDidMount () {
+    const timeRef = firebase.database().ref().child('next_round_start')
+    const roundNumberRef = firebase.database().ref().child('round_number')
+    timeRef.on('value', snap =>
+      this.setState({ roundTimeLeft: snap.val() })
+    )
+    roundNumberRef.on('value', snap =>
+      this.setState({ roundNumber: snap.val() })
+    )
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <h1>{this.state.roundNumber}</h1>
-                <CountdownTimer date={moment(this.state.roundTimeLeft).toDate()}></CountdownTimer>
-            </div>
-            
-        );
-    }
+  render () {
+    return (
+      <div className="App">
+        <h1>{this.state.roundNumber}</h1>
+        <CountdownTimer date={moment(this.state.roundTimeLeft).toDate()}></CountdownTimer>
+      </div>
+
+    )
+  }
 }
 
-export default App;
+export default App
--- client/src/components/CountdownTimer.js
+++ client/src/components/CountdownTimer.js
@@ -1,34 +1,35 @@
-import React, { PropTypes, Component } from 'react';
+// eslint-disable-next-line no-unused-vars
+import React, { Component } from 'react'
 
 export class CountdownTimer extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
     this.state = {
       days: 0,
       hours: 0,
       min: 0,
-      sec: 0,
+      sec: 0
     }
   }
 
-  componentDidMount() {
+  componentDidMount () {
     // update every second
     this.interval = setInterval(() => {
-      const date = this.calculateCountdown(this.props.date);
-      date ? this.setState(date) : this.stop();
-    }, 1000);
+      const date = this.calculateCountdown(this.props.date)
+      date ? this.setState(date) : this.stop()
+    }, 1000)
   }
 
-  componentWillUnmount() {
-    this.stop();
+  componentWillUnmount () {
+    this.stop()
   }
 
-  calculateCountdown(endDate) {
-    let diff = (Date.parse(new Date(endDate)) - Date.parse(new Date())) / 1000;
+  calculateCountdown (endDate) {
+    let diff = (Date.parse(new Date(endDate)) - Date.parse(new Date())) / 1000
 
     // clear countdown when date is reached
-    if (diff <= 0) return false;
+    if (diff <= 0) return false
 
     const timeLeft = {
       years: 0,
@@ -36,52 +37,52 @@ export class CountdownTimer extends Component {
       hours: 0,
       min: 0,
       sec: 0,
-      millisec: 0,
-    };
+      millisec: 0
+    }
 
     // calculate time difference between now and expected date
     if (diff >= (365.25 * 86400)) { // 365.25 * 24 * 60 * 60
-      timeLeft.years = Math.floor(diff / (365.25 * 86400));
-      diff -= timeLeft.years * 365.25 * 86400;
+      timeLeft.years = Math.floor(diff / (365.25 * 86400))
+      diff -= timeLeft.years * 365.25 * 86400
     }
     if (diff >= 86400) { // 24 * 60 * 60
-      timeLeft.days = Math.floor(diff / 86400);
-      diff -= timeLeft.days * 86400;
+      timeLeft.days = Math.floor(diff / 86400)
+      diff -= timeLeft.days * 86400
     }
     if (diff >= 3600) { // 60 * 60
-      timeLeft.hours = Math.floor(diff / 3600);
-      diff -= timeLeft.hours * 3600;
+      timeLeft.hours = Math.floor(diff / 3600)
+      diff -= timeLeft.hours * 3600
     }
     if (diff >= 60) {
-      timeLeft.min = Math.floor(diff / 60);
-      diff -= timeLeft.min * 60;
+      timeLeft.min = Math.floor(diff / 60)
+      diff -= timeLeft.min * 60
     }
-    timeLeft.sec = diff;
+    timeLeft.sec = diff
 
-    return timeLeft;
+    return timeLeft
   }
 
-  stop() {
-    clearInterval(this.interval);
+  stop () {
+    clearInterval(this.interval)
   }
 
-  addLeadingZeros(value) {
-    value = String(value);
+  addLeadingZeros (value) {
+    value = String(value)
     while (value.length < 2) {
-      value = '0' + value;
+      value = '0' + value
     }
-    return value;
+    return value
   }
 
-  render() {
-    const countDown = this.state;
+  render () {
+    const countDown = this.state
 
     return (
       <div className="Countdown">
         <span className="Countdown-col">
           <span className="Countdown-col-element">
-              <strong>{this.addLeadingZeros(countDown.days)}</strong>
-              <span>{countDown.days === 1 ? 'Day' : 'Days'}</span>
+            <strong>{this.addLeadingZeros(countDown.days)}</strong>
+            <span>{countDown.days === 1 ? 'Day' : 'Days'}</span>
           </span>
         </span>
 
@@ -91,7 +92,6 @@ export class CountdownTimer extends Component {
             <span>Hours</span>
           </span>
         </span>
-
 
         <span className="Countdown-col">
           <span className="Countdown-col-element">
@@ -107,11 +107,10 @@ export class CountdownTimer extends Component {
           </span>
         </span>
       </div>
-    );
+    )
   }
 }
 
-
 CountdownTimer.defaultProps = {
   date: new Date()
-};
+}
